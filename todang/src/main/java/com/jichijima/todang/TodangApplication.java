8128da3a88e3c7c1b5db2bba643c766de85cb4d2
송우석 <plutoxxx1014@gmail.com>
2025-01-24 18:09:03 +0900

    init pjt

---
+++ todang/src/main/java/com/jichijima/todang/TodangApplication.java
@@ -0,0 +1,13 @@
+package com.jichijima.todang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodangApplication.class, args);
+    }
+
+}
---
+++ todang/src/test/java/com/jichijima/todang/TodangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jichijima.todang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
