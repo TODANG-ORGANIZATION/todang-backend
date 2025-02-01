508ec97b9d0d85ee5fa35553ae8bb6e67e54e2b1
송우석 <plutoxxx1014@gmail.com>
2025-02-01 20:24:22 +0900

    Feat: User 엔티티 생성 및 기본 기능 추가

--- todang/src/main/java/com/jichijima/todang/model/entity/User.java
+++ todang/src/main/java/com/jichijima/todang/model/entity/User.java
@@ -1,4 +1,36 @@
 package com.jichijima.todang.model.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String tel;
+
+    private String userPhoto;
+
+    @Column(nullable = false, columnDefinition = "TINYINT")
+    private int role;
 }
