ef40ebe5c27f379cd822e324dfef3db209261b8b
방민규 <bangmq@naver.com>
2025-02-04 13:13:34 +0900

    Feat : Order Entity 생성

---
+++ todang/src/main/java/com/jichijima/todang/model/entity/order/Order.java
@@ -0,0 +1,45 @@
+package com.jichijima.todang.model.entity.order;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long restaurantId;
+
+    @Column (nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Short state;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column
+    private Integer estimatedTime;
+
+    @Column(nullable = false)
+    private LocalDateTime orderDatetime;
+
+    @Column
+    private LocalDateTime finishDatetime;
+
+    @Column(length = 300)
+    private String requirement;
+
+    @Column(length = 20)
+    private String tel;
+}
