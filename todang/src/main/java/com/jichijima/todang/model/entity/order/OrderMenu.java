c2069fc274e81a83d9ec1569522d15787c3209e2
방민규 <bangmq@naver.com>
2025-02-04 13:51:49 +0900

    Feat : Order Menu 엔티티 생성

---
+++ todang/src/main/java/com/jichijima/todang/model/entity/order/OrderMenu.java
@@ -0,0 +1,24 @@
+package com.jichijima.todang.model.entity.order;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_menu")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Column (nullable = false)
+    private Long menuId;
+}
